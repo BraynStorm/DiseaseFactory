a85b05ca217ee8be74a5c31c301bbc154f8ec93d
BraynStorm <glav0r3zzz4@gmail.com>
2013-10-04 19:13:13 +0300

    .GitIgnore

--- js/main.js
+++ js/main.js
@@ -3,18 +3,30 @@ Game.FIXCSS = {};
 Game.SaveManager = {};
 
 Game.Data = {
+    //Constants
     const: {
         COOKIE_STRING: 'Game_DiseaseFactory_cookie'
+    },
+    //Selectors IKR...
+    Selectors: {
+        divMAIN: "#MAIN"
     },
     playerName: ''
 };
 
+Game.LoadPage = function(page) {
+    $.ajax({
+        url: 'includes/' + page,
+        success: function(data) {
 
+        }
+    });
+};
 
 
 Game.SaveManager.Save = function() {
-    var DATA;
-    $.cookie('Game_DiseaseFactory_cookie', encode64(DATA), {expires: 366});
+    var DATA =
+        $.cookie('Game_DiseaseFactory_cookie', encode64(DATA), {expires: 366});
 };
 
 
